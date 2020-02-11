2c050dce9993cb0e2a69cd4892dbc562e8ec03ce
Everett Yeaw <evyeaw@gmail.com>
2020-02-11 13:56:29 -0500

    refactored format and removed superfluous element

--- app/javascript/containers/PlanetTiles.js
+++ app/javascript/containers/PlanetTiles.js
@@ -4,10 +4,8 @@ const PlanetTiles = ({ planet }) => {
   let { id, name, population } = planet
 return(
   <div>
-    <p>
       <li key={id}> Name:{name} || Location:{population}</li>
-    </p>
   </div>
-)
+  )
 }
 export default PlanetTiles
