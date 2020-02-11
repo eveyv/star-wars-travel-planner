445d07e30b64e24a913f59026da5b272cd057956
Everett Yeaw <evyeaw@gmail.com>
2020-02-11 13:57:03 -0500

    changed nested status but page broke due to uninitialized constnat Planet. reverted

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -2,6 +2,6 @@ class PlanetsController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
 
   def show
-    render json: { planets: Planet.all}
+    render json: { planets: Planet.all }
   end
 end
