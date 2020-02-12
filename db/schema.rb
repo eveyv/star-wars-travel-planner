38236c2bb0c926147810d87740f78e37091704c1
Everett Yeaw <evyeaw@gmail.com>
2020-02-12 13:30:25 -0500

    Schema changes with migration rollback migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_212123) do
+ActiveRecord::Schema.define(version: 2020_02_11_212221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "planets", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "terrain", null: false
+    t.string "url", null: false
+    t.string "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
