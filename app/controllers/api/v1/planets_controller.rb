1321ebab904a97b307bbf1d084208f618f3bdfd8
Max <max.lee.092691@gmail.com>
2020-02-13 12:06:27 -0500

    Added show page

--- app/controllers/api/v1/planets_controller.rb
+++ app/controllers/api/v1/planets_controller.rb
@@ -2,7 +2,13 @@ class Api::V1::PlanetsController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
 
   def index
+    render json: Planet.all
+  end
 
+  def show
+    planet = Planet.find(params[:id])
+    render json: planet
+    binding.pry
   end
 
 end
