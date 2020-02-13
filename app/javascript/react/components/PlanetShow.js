47915293c318ec870a41506d2793c871a4ed6d4e
Everett Yeaw <evyeaw@gmail.com>
2020-02-13 16:59:56 -0500

    edited syntax

--- app/javascript/react/components/PlanetShow.js
+++ app/javascript/react/components/PlanetShow.js
@@ -1,19 +1,15 @@
 import React from 'react'
-
 const PlanetShow = ({ planetObject }) => {
   let { id, name, terrain, url, description, destination_url } = planetObject
+  
   return(
     <div className="stars">
-      <h2>please sweet jesus work</h2>
-      <div className="planet-tile">
-      {name}
-      <img src={destination_url}/>
-      </div>
-      <div className="planet-tile">
-      {description}
-      </div>
+      <h2>| {name} |</h2>
+        <img className="destination-pic" src={destination_url}/>
+          <div className="planet-description">
+            {description}
+          </div>
     </div>
   )
 }
-
 export default PlanetShow;
