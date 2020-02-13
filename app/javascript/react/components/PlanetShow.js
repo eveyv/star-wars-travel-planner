e047f412f978c39171ac88a127e5c1f8c4d95847
Max <max.lee.092691@gmail.com>
2020-02-13 16:01:02 -0500

    Filled out show page a little more

--- app/javascript/react/components/PlanetShow.js
+++ app/javascript/react/components/PlanetShow.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
 const PlanetShow = ({ planetObject }) => {
-  let { id, name, terrain, url, description } = planetObject
-
+  let { id, name, terrain, url, description, destination_url } = planetObject
   return(
     <div className="stars">
       <h2>please sweet jesus work</h2>
-      <div className="planet-tile"> Name: {name}</div>
+      <div className="planet-tile">
+      {name}
+      <img src={destination_url}/>
+      </div>
+      <div className="planet-tile">
+      {description}
+      </div>
     </div>
   )
 }
