ab913b01900c5d37dbf8bb3c1fa98c6622ea1f71
Max <max.lee.092691@gmail.com>
2020-02-13 16:00:18 -0500

    Included link from index page to show page

--- app/javascript/react/components/PlanetTiles.js
+++ app/javascript/react/components/PlanetTiles.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PlanetTiles = ({ planet }) => {
-  let { id, name, terrain, planet_url, description } = planet
+  let { id, name, terrain, planet_url, description, destination_url } = planet
 return(
   <div className="tiles">
       <div key={id} className="planet-tile">
-        | {name} |
+        <Link to={`/planets/${planet.id}`}>| {name} |</Link>
         <img src={planet_url}/>
       </div>
   </div>
   )
 }
+
 export default PlanetTiles
