b5778be1fc7ca85d0cf3c3c62fcde5d51e53f5a3
Max <max.lee.092691@gmail.com>
2020-02-13 12:06:39 -0500

    Added for Showpage

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import PlanetsIndexContainer from './PlanetsIndexContainer'
+import PlanetShowContainer from './PlanetShowContainer'
 
 export const App = (props) => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={PlanetsIndexContainer} />
+        <Route exact path="/planets/:id" component={PlanetShowContainer} />
       </Switch>
     </BrowserRouter>
   )
