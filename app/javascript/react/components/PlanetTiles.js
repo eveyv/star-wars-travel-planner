d3cf10c50902f565620edd313a6ba84e06b7e23e
Everett Yeaw <evyeaw@gmail.com>
2020-02-14 11:45:58 -0500

    added classname for styling

--- app/javascript/react/components/PlanetTiles.js
+++ app/javascript/react/components/PlanetTiles.js
@@ -5,12 +5,10 @@ const PlanetTiles = ({ planet }) => {
   let { id, name, terrain, planet_url, description } = planet
 
 return(
-  <div className="grid-x">
       <div key={id}>
-        <Link to={`/planets/${planet.id}`}>| {name} |</Link>
+        <Link to={`/planets/${planet.id}`} className="planet-names">| {name} |</Link>
         <img className="planet-pics" src={planet_url} />
       </div>
-    </div>
   )
 }
 export default PlanetTiles
