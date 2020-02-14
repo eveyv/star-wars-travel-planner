8475b55cbaa491825fe24b7fd545b3d10a7f92fb
Everett Yeaw <evyeaw@gmail.com>
2020-02-14 10:36:40 -0500

    worked with className for styling

--- app/javascript/react/components/PlanetTiles.js
+++ app/javascript/react/components/PlanetTiles.js
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom'
 
 const PlanetTiles = ({ planet }) => {
   let { id, name, terrain, planet_url, description } = planet
+
 return(
-      <div key={id} className="columns small-1 large-4">
+  <div className="grid-x">
+      <div key={id}>
         <Link to={`/planets/${planet.id}`}>| {name} |</Link>
         <img className="planet-pics" src={planet_url} />
       </div>
+    </div>
   )
 }
 export default PlanetTiles
