eb97214dbff1ff043061b030ea72e0f3451e1074
Max <max.lee.092691@gmail.com>
2020-02-18 11:15:25 -0500

    included routes in app.js for trip component

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import PlanetsIndexContainer from './PlanetsIndexContainer'
 import PlanetShowContainer from './PlanetShowContainer'
+import TripsIndexContainer from './TripsIndexContainer'
 
 export const App = (props) => {
   return (
@@ -9,6 +10,9 @@ export const App = (props) => {
       <Switch>
         <Route exact path="/" component={PlanetsIndexContainer} />
         <Route exact path="/planets/:id" component={PlanetShowContainer} />
+        <Route exact path="/planets" component={PlanetsIndexContainer} />
+        <Route exact path="/trips" component={TripsIndexContainer} />
+
       </Switch>
     </BrowserRouter>
   )
