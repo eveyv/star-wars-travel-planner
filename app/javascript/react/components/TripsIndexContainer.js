e2ccae290bf3a91ef9d1d971ae90c52a388cd34e
Max <max.lee.092691@gmail.com>
2020-02-18 11:14:36 -0500

    Just set up header to make sure it was rendering on /trips

---
+++ app/javascript/react/components/TripsIndexContainer.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect} from 'react'
+import App from './App'
+
+const TripsIndexContainer = props => {
+
+
+  return(
+    <div className="stars">
+      <h2>The Last Jedi is an abomination</h2>
+    </div>
+  )
+}
+
+export default TripsIndexContainer
