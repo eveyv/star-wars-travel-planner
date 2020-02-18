5b1734b8a1da092b754613636452f623a80e4a40
Everett Yeaw <evyeaw@gmail.com>
2020-02-18 16:30:02 -0500

    wip form for trips

--- app/javascript/react/components/TripForm.js
+++ app/javascript/react/components/TripForm.js
@@ -4,26 +4,34 @@ class TripForm = (props) => {
 
   return(
     <form>
-
       <label>
         Trip Name:
-        <input type="text" name="trip name" value="" />
-      </label>
-      <label>
-        First Destination:
-        <input type="text" name="first destination" value="" />
-      </label>
-      <label>
-        Second Destination:
-        <input type="text" name="second destination" value="" />
+        <input
+        type="text"
+        name="trip-name"
+        value=""
+        />
       </label>
 
+      <select className="Choose your first planet:">
+        <option value="Hoth">Hoth</option>
+        <option value="Coruscant">Coruscant</option>
+        <option value="Death Star">Death Star</option>
+        <option value="Tatooine">Tatooine</option>
+        <option value="Kashyyk">Kashyyk</option>
+        <option value="Mustafar">Mustafar</option>
+      </select>
+
+      <select className="Choose your second planet:">
+        <option value="Hoth">Hoth</option>
+        <option value="Coruscant">Coruscant</option>
+        <option value="Death Star">Death Star</option>
+        <option value="Tatooine">Tatooine</option>
+        <option value="Kashyyk">Kashyyk</option>
+        <option value="Mustafar">Mustafar</option>
+      </select>
     </form>
-
-
-
   )
-
 }
 
 export default TripForm;
