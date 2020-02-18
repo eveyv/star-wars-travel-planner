19d0198af6ea42525c3100a3fa7b9d647bbe7754
Max <max.lee.092691@gmail.com>
2020-02-18 16:37:50 -0500

    Included fetch request

--- app/javascript/react/components/TripsIndexContainer.js
+++ app/javascript/react/components/TripsIndexContainer.js
@@ -1,12 +1,44 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import App from './App'
+import TripTiles from './TripTiles'
 
 const TripsIndexContainer = props => {
+  const [trips, setTrips] = useState([])
 
+  useEffect(() => {
+    fetch('/api/v1/trips.json')
+    .then(response => {
+      if (response.ok) {
+        return response
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+          error = new Error(errorMessage);
+          throw(error)
+      }
+    })
+    .then(response => {
+      return response.json()
+    })
+    .then(body => {
+      setTrips(body)
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`))
+  }, [])
+
+  const tripTiles = trips.map(trip => {
+
+    return(
+      <TripTiles
+        key={trip.id}
+        trip={trip}
+      />
+    )
+  })
 
   return(
     <div className="stars">
       <h2>The Last Jedi is an abomination</h2>
+        {tripTiles}
     </div>
   )
 }
