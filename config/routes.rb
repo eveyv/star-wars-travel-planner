ce453c1a368fb73460e85faab0222f502fec63e0
Max <max.lee.092691@gmail.com>
2020-02-18 11:12:24 -0500

    Set up routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
 
+  get "/trips", to: 'homes#index'
   get "/planets", to: 'homes#index'
   get "/planets/:id", to: 'homes#index'
 
   namespace :api do
     namespace :v1 do
-      resources :planets, only: [:index, :show]
+      resources :planets, only: [:index, :show] do
+        resources :trips, only: [:index]
+        end
+      end
     end
   end
-end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
