e2d6626b86efc31e937eb432ab70fc8207228ca7
Max <max.lee.092691@gmail.com>
2020-02-18 11:12:08 -0500

    Had to create new trip to seed in

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@trips = Trip.create([
+    { name: "Vacation"
+    },
+])
+
 @planets = Planet.create([
     { name: 'Hoth',
       terrain: 'Icy',
