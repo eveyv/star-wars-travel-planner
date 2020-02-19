67a505a1198bce0674ad467931053166876052dd
Everett Yeaw <evyeaw@gmail.com>
2020-02-19 17:09:59 -0500

    worked on form for trip creation

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -4,4 +4,17 @@ class Api::V1::TripsController < ApplicationController
   def index
     render json: Trip.all
   end
+
+  def create
+    trip = Trip.new(trip_params)
+    trip.user = current_user
+    if trip.save
+      render json: { trips: trip }
+    else
+      render json: { error: trip.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+
+
 end
