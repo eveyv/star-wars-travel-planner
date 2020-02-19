50c7bbeb5cf6175dbf7d1048f55ea682418ab8b7
Everett Yeaw <evyeaw@gmail.com>
2020-02-19 14:09:34 -0500

    changed planet show images

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,13 @@
       terrain: 'Icy',
       planet_url: 'https://vignette.wikia.nocookie.net/battlefront/images/9/99/Rhen_var.jpg/revision/latest?cb=20110815204106',
       description: 'A remote planet at the far reaches of the solar system.  Enjoy the sprawling fields of ice and then have some hot cocoa inside one of its famous research facilities!',
-      destination_url: 'https://static0.srcdn.com/wordpress/wp-content/uploads/2019/08/star-wars-empire-strikes-back-hoth-ion-canon-echo-base-Edited.jpg'
+      destination_url: 'https://vignette.wikia.nocookie.net/starwars/images/0/0a/HothATATs-SWB.png/revision/latest?cb=20160220045625'
      },
      { name: 'Coruscant',
        terrain: 'Metropolitan',
        planet_url: 'https://vignette.wikia.nocookie.net/starwars/images/1/16/Coruscant-EotE.jpg/revision/latest?cb=20130908205521',
        description: 'The famed city-planet. If New York never sleeps, then Coruscant must be the most tired of all the cities!',
-       destination_url: 'https://i.ytimg.com/vi/9ajkFR-Hwjc/maxresdefault.jpg'
+       destination_url: 'https://vignette.wikia.nocookie.net/starwars/images/5/5c/Coruscant_at_night.jpg/revision/latest?cb=20080610031227'
       },
       { name: 'Death Star',
         terrain: 'Mechanical',
@@ -28,7 +28,7 @@
          terrain: 'Desert',
          planet_url: 'https://vignette.wikia.nocookie.net/empireatwar/images/8/82/Tatooine-TOR.jpg/revision/latest?cb=20150126022416',
          description: 'Visit the beautiful desert planet home to the most beloved family in the universe, the Skywalkers!',
-         destination_url: 'https://www.wallpaperup.com/uploads/wallpapers/2012/10/17/19682/41481aee5148c042c09f9cf1e77d64e6-700.jpg'
+         destination_url: 'https://vignette.wikia.nocookie.net/starwars/images/e/e6/MosEisley-celebration.png/revision/latest?cb=20130420052326'
         },
         { name: 'Kashyyk',
           terrain: 'Forrest',
@@ -40,6 +40,6 @@
            terrain: 'Volcanic',
            planet_url: 'https://vignette.wikia.nocookie.net/starwars/images/b/b2/Mustafar_FFGRebellion.png/revision/latest?cb=20190819193802',
            description: 'This obviously evil planet might not seem too inviting but there are few better places in the galaxy where you can tan with such blazing efficiency!',
-           destination_url: 'https://lumiere-a.akamaihd.net/v1/images/databank_mustafar_01_169_5b470758.jpeg?region=0%2C98%2C1560%2C780'
+           destination_url: 'https://vignette.wikia.nocookie.net/starwars/images/4/4a/Lava_Rivers.png/revision/latest?cb=20140321174433'
           },
   ])
