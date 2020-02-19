f692f2f4d5bfb84bc1a7b4b5cdbb1ff3a32ed63d
Everett Yeaw <evyeaw@gmail.com>
2020-02-19 16:00:05 -0500

    building out form functionality

--- app/javascript/react/components/TripForm.js
+++ app/javascript/react/components/TripForm.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-class TripForm = (props) => {
+const TripForm = props => {
 
   return(
-    <form>
-      <label>
+
+    <form className="trip-form">
+      <label className="form-text">
         Trip Name:
         <input
         type="text"
@@ -13,23 +14,46 @@ class TripForm = (props) => {
         />
       </label>
 
-      <select className="Choose your first planet:">
-        <option value="Hoth">Hoth</option>
-        <option value="Coruscant">Coruscant</option>
-        <option value="Death Star">Death Star</option>
-        <option value="Tatooine">Tatooine</option>
-        <option value="Kashyyk">Kashyyk</option>
-        <option value="Mustafar">Mustafar</option>
-      </select>
+      <label className="form-text">
+        Trip Description:
+        <input
+        type="text"
+        name="trip-name"
+        value=""
+        />
+      </label>
 
-      <select className="Choose your second planet:">
-        <option value="Hoth">Hoth</option>
-        <option value="Coruscant">Coruscant</option>
-        <option value="Death Star">Death Star</option>
-        <option value="Tatooine">Tatooine</option>
-        <option value="Kashyyk">Kashyyk</option>
-        <option value="Mustafar">Mustafar</option>
-      </select>
+        <label className="form-text">
+          Select your first planet:
+          <select>
+            <option disabled selected value> -- select an option -- </option>
+            <option value="Hoth">Hoth</option>
+            <option value="Coruscant">Coruscant</option>
+            <option value="Death Star">Death Star</option>
+            <option value="Tatooine">Tatooine</option>
+            <option value="Kashyyk">Kashyyk</option>
+            <option value="Mustafar">Mustafar</option>
+          </select>
+        </label>
+
+        <label className="form-text">
+            Select your second planet:
+          <select>
+            <option disabled selected value> -- select an option -- </option>
+            <option value="Hoth">Hoth</option>
+            <option value="Coruscant">Coruscant</option>
+            <option value="Death Star">Death Star</option>
+            <option value="Tatooine">Tatooine</option>
+            <option value="Kashyyk">Kashyyk</option>
+            <option value="Mustafar">Mustafar</option>
+          </select>
+        </label>
+
+        <input
+          type="submit"
+          className="btn"
+          />
+
     </form>
   )
 }
