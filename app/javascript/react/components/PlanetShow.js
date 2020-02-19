f00230026103c8c81548655c0ff6534048329e9c
Everett Yeaw <evyeaw@gmail.com>
2020-02-19 14:44:18 -0500

    fixed formatting

--- app/javascript/react/components/PlanetShow.js
+++ app/javascript/react/components/PlanetShow.js
@@ -5,23 +5,10 @@ const PlanetShow = ({ planetObject }) => {
   return(
     <div>
       <h2>| {name} |</h2>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> cafe9115409afe56e740b75b4b4c754315fee6d2
       <img className="destination-pic" src={destination_url}/>
       <div className="planet-description">
         {description}
       </div>
-<<<<<<< HEAD
-=======
-        <img className="destination-pic" src={destination_url}/>
-        <div className="planet-description">
-            {description}
-        </div>
->>>>>>> 92b45ebcd4598b7721c7af546df266fa7c691cce
-=======
->>>>>>> cafe9115409afe56e740b75b4b4c754315fee6d2
     </div>
   )
 }
