1f38d3e7cbc28a4f4ecda5f26aa8b94f42676af1
Everett Yeaw <evyeaw@gmail.com>
2020-02-20 13:59:43 -0500

    added functionality for posting and persisting trips

--- app/javascript/react/components/TripsIndexContainer.js
+++ app/javascript/react/components/TripsIndexContainer.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import App from './App'
+import { Redirect } from 'react-router-dom'
 import TripTiles from './TripTiles'
 import TripForm from './TripForm'
 
 const TripsIndexContainer = props => {
-
+  const [shouldRedirect, setShouldRedirect] = useState(false)
   const [trips, setTrips] = useState([])
-
+  let [error, setError ] = useState([])
   useEffect(() => {
     fetch('/api/v1/trips.json')
     .then(response => {
@@ -15,7 +16,7 @@ const TripsIndexContainer = props => {
       } else {
         let errorMessage = `${response.status} (${response.statusText})`,
           error = new Error(errorMessage);
-          throw(error)
+          throw error
       }
     })
     .then(response => {
@@ -28,35 +29,47 @@ const TripsIndexContainer = props => {
   }, [])
 
   const setNewTrip = (formPayload) => {
-  fetch('/api/v1/trips', {
-    method: "POST",
-    body: JSON.stringify(formPayload),
-  })
-  .then(response => {
-    if (response.ok) {
-      return response
-    } else {
-      let errorMessage = `${response.status} (${response.statusText})`
-      error = new Error(errorMessage)
-      throw(error)
-    }
-  })
-  .then(response => response.json())
-  .then(body => {
-    setTrips([
-      ...trips,
-      body
-    ])
-  })
-  .catch(error => console.error(`Error in fetch: ${error.message}`))
+
+  fetch('/api/v1/trips.json', {
+      method: "POST",
+      body: JSON.stringify(formPayload),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        return response
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`
+        error = new Error(errorMessage)
+        throw error
+      }
+    })
+    .then(response => response.json())
+    .then(body => {
+      setTrips([
+        ...trips,
+        body])
+      setShouldRedirect(true)
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`))
 }
 
-  const tripTiles = trips.map(trip => {
+if(shouldRedirect) {
+  return <Redirect to ={`/trips/${tripID}`} />
+}
 
+
+  const tripTiles = trips.map(trip => {
     return(
       <TripTiles
         key={trip.id}
         trip={trip}
+        name={trip.name}
+        descripton={trip.description}
+
       />
     )
   })
