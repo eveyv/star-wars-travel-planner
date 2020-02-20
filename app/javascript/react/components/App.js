e9c6302566f33ed7ae1782239c33159cfe768731
Everett Yeaw <evyeaw@gmail.com>
2020-02-20 16:08:00 -0500

    added route for new trip

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import PlanetsIndexContainer from './PlanetsIndexContainer'
 import PlanetShowContainer from './PlanetShowContainer'
 import TripsIndexContainer from './TripsIndexContainer'
+import NewTrip from './NewTrip'
 
 export const App = (props) => {
   return (
@@ -11,6 +12,7 @@ export const App = (props) => {
         <Route exact path="/" component={PlanetsIndexContainer} />
         <Route exact path="/planets" component={PlanetsIndexContainer} />
         <Route exact path="/trips" component={TripsIndexContainer} />
+        <Route exact path="/trips/new" component={NewTrip} />
         <Route exact path="/planets/:id" component={PlanetShowContainer} />
       </Switch>
     </BrowserRouter>
