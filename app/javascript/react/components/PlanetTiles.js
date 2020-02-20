31827ec509231447f4f3a427ee03557278ef17cb
Max <max.lee.092691@gmail.com>
2020-02-20 14:14:29 -0500

    styling changes to planettiles

--- app/javascript/react/components/PlanetTiles.js
+++ app/javascript/react/components/PlanetTiles.js
@@ -2,15 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const PlanetTiles = ({ planet }) => {
-  let { id, name, terrain, planet_url, description, destination_url } = planet
+  let { id, name, terrain, planet_url, description } = planet
+
 return(
-  <div className="tiles">
-      <div key={id} className="planet-tile">
-        <Link to={`/planets/${planet.id}`}>| {name} |</Link>
-        <img className="planet-pics" src={planet_url}/>
-      </div>
+  <div className="cell large-4 medium-6 small-12" key={id}>
+    <Link to={`/planets/${planet.id}`} className="planet-names">| {name} |</Link>
+    <img className="planet-pics" src={planet_url} />
   </div>
+
   )
 }
-
 export default PlanetTiles
