d96acb90648fddc63a69d8635f6d3fbe16f53b48
Everett Yeaw <evyeaw@gmail.com>
2020-02-20 13:59:15 -0500

    added state for ability to properly submit form

--- app/javascript/react/components/TripForm.js
+++ app/javascript/react/components/TripForm.js
@@ -1,41 +1,29 @@
-import React, { useState}  from 'react'
+import React, { useState }  from 'react'
+import _ from "lodash"
+import ErrorList from "./ErrorList"
 
 const TripForm = props => {
-
-const [ newTrip, setNewTrip ] = useState({
+  const [ errors, setErrors ] = useState({})
+  const [ newTrip, setNewTrip ] = useState({
     name: "",
     description: "",
-    planetOne: "",
-    planetTwo: "",
+    // planetOne: "",
+    // planetTwo: "",
 })
 
-const [ errors, setErrors ] = useState({})
 
 const handleInputChange = event => {
   setNewTrip({
     ...newTrip,
-    [event.currentTarget.name]: event.currentTarget.value
+    [event.currentTarget.id]: event.currentTarget.value
   })
 }
 
-const onSubmit = (event) => {
-    event.preventDefault()
-    if (validFormSubmission()) {
-      props.onSubmit(newTrip)
-      setNewTrip({
-        name: "",
-        description: "",
-        planetOne: "",
-        planetTwo: "",
-      })
-    }
-  }
-
 const validFormSubmission = () => {
   let submitErrors = {}
-  const requiredFields = [ "name", "description", "planetOne", "planetTwo" ]
+  const requiredFields = [ "name", "description" ]
     requiredFields.forEach(field => {
-      if (newTrip[field].trim() === "") {
+      if(newTrip[field].trim() === "") {
         submitErrors = {
         ...submitErrors,
         [field]: "cannot be blank"
@@ -46,12 +34,27 @@ const validFormSubmission = () => {
   return _.isEmpty(submitErrors)
 }
 
-  return(
+const handleSubmit = (event) => {
+    event.preventDefault()
+    let formPayload = newTrip;
+    if (validFormSubmission()) {
+      props.onSubmit(formPayload)
+      setNewTrip({
+        name: "",
+        description: "",
+        // planetOne: "",
+        // planetTwo: "",
+      })
+    }
+  }
 
-    <form className="trip-form">
+  return(
+    <form className="trip-form" onSubmit={handleSubmit}>
+      <ErrorList errors={errors} />
       <label className="form-text">
         Trip Name:
         <input
+        id="name"
         type="text"
         name="name"
         value={newTrip.name}
@@ -62,56 +65,57 @@ const validFormSubmission = () => {
       <label className="form-text">
         Trip Description:
         <input
+        id="description"
         type="text"
         name="description"
         value={newTrip.description}
         onChange={handleInputChange}
         />
       </label>
-
-        <label className="form-text">
-          Select your first planet:
-          <select
-            id="planetOne"
-            name="planetOne"
-            value={newTrip.planetOne}
-            onChange={handleInputChange}
-            >
-            <option disabled selected value> -- select an option -- </option>
-            <option value="Hoth">Hoth</option>
-            <option value="Coruscant">Coruscant</option>
-            <option value="Death Star">Death Star</option>
-            <option value="Tatooine">Tatooine</option>
-            <option value="Kashyyk">Kashyyk</option>
-            <option value="Mustafar">Mustafar</option>
-          </select>
-        </label>
-
-      <label className="form-text">
-        Select your second planet:
-          <select
-            id="planetTwo"
-            name="planetTwo"
-            value={newTrip.planetTwo}
-            onChange={handleInputChange}
-            >
-
-            <option disabled selected value> -- select an option -- </option>
-            <option value="Hoth">Hoth</option>
-            <option value="Coruscant">Coruscant</option>
-            <option value="Death Star">Death Star</option>
-            <option value="Tatooine">Tatooine</option>
-            <option value="Kashyyk">Kashyyk</option>
-            <option value="Mustafar">Mustafar</option>
-        </select>
-      </label>
-
-        <input
-          type="submit"
-          value="submit"
-          className="btn"
-          />
+      <input
+      type="submit"
+      value="submit"
+      className="btn"
+      />
     </form>
+
+      //   <label className="form-text">
+      //     Select your first planet:
+      //     <select
+      //       id="planetOne"
+      //       name="planetOne"
+      //       value={newTrip.planetOne}
+      //       onChange={handleInputChange}
+      //       >
+      //       <option disabled selected value> -- select an option -- </option>
+      //       <option value="Hoth">Hoth</option>
+      //       <option value="Coruscant">Coruscant</option>
+      //       <option value="Death Star">Death Star</option>
+      //       <option value="Tatooine">Tatooine</option>
+      //       <option value="Kashyyk">Kashyyk</option>
+      //       <option value="Mustafar">Mustafar</option>
+      //     </select>
+      //   </label>
+      //
+      // <label className="form-text">
+      //   Select your second planet:
+      //     <select
+      //       id="planetTwo"
+      //       name="planetTwo"
+      //       value={newTrip.planetTwo}
+      //       onChange={handleInputChange}
+      //       >
+      //
+      //       <option disabled selected value> -- select an option -- </option>
+      //       <option value="Hoth">Hoth</option>
+      //       <option value="Coruscant">Coruscant</option>
+      //       <option value="Death Star">Death Star</option>
+      //       <option value="Tatooine">Tatooine</option>
+      //       <option value="Kashyyk">Kashyyk</option>
+      //       <option value="Mustafar">Mustafar</option>
+      //   </select>
+      // </label>
+
   )
 }
 
