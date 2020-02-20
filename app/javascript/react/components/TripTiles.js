af22340b7ce07d8c0380eadd04f4ea859e15e125
Max <max.lee.092691@gmail.com>
2020-02-20 14:17:46 -0500

    Added planet_trip

--- app/javascript/react/components/TripTiles.js
+++ app/javascript/react/components/TripTiles.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const TripTiles = ({ trip }) => {
-  let { id, name } = trip
+  let { id, name, planet_name } = trip
+
   return(
     <div>
       <div key={id} className="planet-description">
         {name}
+          <li>{planet_name}</li>
       </div>
     </div>
     )
