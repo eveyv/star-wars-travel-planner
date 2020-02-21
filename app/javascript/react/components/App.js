c95531435c126e7b5361d267403c1a234cee8745
Everett Yeaw <evyeaw@gmail.com>
2020-02-21 09:28:21 -0500

    added relevant imports for TripShow

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -4,6 +4,7 @@ import PlanetsIndexContainer from './PlanetsIndexContainer'
 import PlanetShowContainer from './PlanetShowContainer'
 import TripsIndexContainer from './TripsIndexContainer'
 import NewTrip from './NewTrip'
+import TripShow from './TripShow'
 
 export const App = (props) => {
   return (
@@ -14,6 +15,7 @@ export const App = (props) => {
         <Route exact path="/trips" component={TripsIndexContainer} />
         <Route exact path="/trips/new" component={NewTrip} />
         <Route exact path="/planets/:id" component={PlanetShowContainer} />
+        <Route exact path="/trips/:trip_id" component={TripShow} />
       </Switch>
     </BrowserRouter>
   )
