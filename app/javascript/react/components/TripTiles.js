ca3d99e3a93e15b5757d829f1728ff6a95a973af
Everett Yeaw <evyeaw@gmail.com>
2020-02-21 09:45:37 -0500

    index list into links to dynamic show pages

--- app/javascript/react/components/TripTiles.js
+++ app/javascript/react/components/TripTiles.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const TripTiles = ({ trip }) => {
   let { id, name } = trip
   return(
     <div key={id} className="planet-description">
-      {name}
+        <Link to={`/trips/${trip.id}`} className="link-to"> {trip.name} </Link>
     </div>
   )
 }
