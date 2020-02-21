38b76acf9390b613356b20720ca86af88e391707
Everett Yeaw <evyeaw@gmail.com>
2020-02-21 16:57:03 -0500

    props being passed down properly

--- app/javascript/react/components/TripTiles.js
+++ app/javascript/react/components/TripTiles.js
@@ -1,18 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 const TripTiles = ({ trip }) => {
-  let { id, name, planet_name } = trip
+  let { id, name, description, } = trip
+
+  const [trips, setTrips] = useState([])
+  let [error, setError ] = useState([])
+
+  useEffect(() => {
+    fetch('/api/v1/trips.json')
+    .then(response => {
+      if (response.ok) {
+        return response
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+          error = new Error(errorMessage);
+          throw error
+      }
+    })
+    .then(response => {
+      return response.json()
+    })
+    .then(body => {
+      setTrips(body)
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`))
+  }, [])
+
+
+  const tripInfo = trips.map(trip => {
+    return
+    <div>
+      <TripShow
+        trip={trip}
+        key={trip.id}
+        name={trip.name}
+        description={trip.description}
+        tripInfo={tripInfo}
+       />
+    </div>
+  })
 
   return(
-    <div key={id} className="planet-description">
-<<<<<<< HEAD
+    <div className="cell large-6">
+      <div className="planet-description" key={id}>
         <Link to={`/trips/${trip.id}`} className="link-to"> {trip.name} </Link>
-=======
-      {name} - {planet_name}
->>>>>>> c5e614e256e4b80f69b9644ab56017c64347661a
+      </div>
     </div>
   )
 }
+
 
 export default TripTiles
